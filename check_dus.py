9e45edd61620b83f93346b3bef689e7e93e5d013
ct <ct@gocept.com>
2013-09-10 11:48:37 +0200

    Trying to get better output ... but doesn't work. No idea why even after reading the library code ... :( sad panda

--- check_dus.py
+++ check_dus.py
@@ -1,6 +1,6 @@
-#!/root/check_dus/bin/python
+#!/opt/check_dus/bin/python
 """Hello world Nagios check."""
-
+import argparse
 from nagiosplugin.result import Result
 from nagiosplugin.state import Ok, Critical
 import hashlib
@@ -10,13 +10,17 @@ import requests
 
 class Equal(nagiosplugin.Context):
 
-    def __init__(self, name, expect, fmt_metric=None, result_cls=Result):
-        super(Equal, self).__init__(name, fmt_metric, result_cls)
+    def __init__(self, name, expect, result_cls=Result):
+        super(Equal, self).__init__(name, None, result_cls)
         self.expect = expect
 
     def evaluate(self, metric, resource):
         result = Ok if metric.value == self.expect else Critical
         return self.result_cls(result, metric=metric)
+
+    def describe(self, metric):
+        # XXX no idea how to get this working
+        return 'got {}, expected {}'.format(metric.value, self.expect)
 
 
 class DUSSIPStatus(nagiosplugin.Resource):
@@ -28,11 +32,16 @@ class DUSSIPStatus(nagiosplugin.Resource):
         return [nagiosplugin.Metric('status_hash', m.hexdigest(), context='account_status')]
 
 
+@nagiosplugin.guarded
 def main():
+    argp = argparse.ArgumentParser()
+    argp.add_argument('-v', '--verbose', action='count', default=0)
+    args = argp.parse_args()
     check = nagiosplugin.Check(
         DUSSIPStatus(),
         Equal('account_status', '9385bc51ed8b0e054a87000dd09cc28e'))
-    check.main()
+    check.main(args.verbose)
+
 
 if __name__ == '__main__':
     main()
